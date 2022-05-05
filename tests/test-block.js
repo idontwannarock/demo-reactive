4c44dfa8922f17b81d35f32443e694f7adaf033c
Howard Wang <idontwannarock@gmail.com>
2022-05-05 18:11:52 +0800

    feat: add docker support for postgres, influxdb, and grafana

---
+++ tests/test-block.js
@@ -0,0 +1,23 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+import { Counter } from "k6/metrics";
+
+let ErrorCount = new Counter("errors");
+
+export const options = {
+  vus: 10,
+  duration: "15s",
+  thresholds: {
+    errors: ["count<10"]
+  }
+};
+
+export default function() {
+  let res = http.get(`http://localhost:9091/reservations`);
+  let success = check(res, {
+    "status is 200": r => r.status === 200
+  });
+  if (!success) {
+    ErrorCount.add(1);
+  }
+}
